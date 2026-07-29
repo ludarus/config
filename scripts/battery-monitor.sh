a553fd21837de73264e9a2e59a365203c8a90f21
ludarus <burgermanjoe48@gmail.com>
2026-07-29 12:14:34 -0400

    low battery notification service

---
+++ scripts/battery-monitor.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+BATTERY=$(< /sys/class/power_supply/BAT0/capacity)
+STATUS=$(< /sys/class/power_supply/BAT0/status)
+
+# Only notify while discharging
+if [ "$STATUS" = "Discharging" ]; then
+    if [ "$BATTERY" -le 15 ]; then
+        notify-send -u critical -t 0 \
+            "Battery Critical" \
+            "Battery is at ${BATTERY}%, connect to AC power immediately"
+    elif [ "$BATTERY" -le 20 ]; then
+        notify-send -u normal \
+            "Battery Low" \
+            "Battery is at ${BATTERY}%."
+    fi
+fi
