e8e9003268a2f7fb8dd8d00461e0c47ea0ecd676
ludarus <burgermanjoe48@gmail.com>
2026-09-03 16:29:07 -0400

    smooth cursor kitty

--- scripts/battery-monitor.sh
+++ scripts/battery-monitor.sh
@@ -6,11 +6,11 @@ STATUS=$(< /sys/class/power_supply/BAT0/status)
 # Only notify while discharging
 if [ "$STATUS" = "Discharging" ]; then
     if [ "$BATTERY" -le 15 ]; then
-        notify-send -u critical -t 0 \
+        # notify-send -u critical -t 0 \
             "Battery Critical" \
             "Battery is at ${BATTERY}%, connect to AC power immediately"
     elif [ "$BATTERY" -le 25 ]; then
-        notify-send -u normal \
+        # notify-send -u normal \
             "Battery Low" \
             "Battery is at ${BATTERY}%."
     fi
