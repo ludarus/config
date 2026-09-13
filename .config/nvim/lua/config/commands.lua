09ae4d1df2ca83b4fe036dd506e6396386224e62
ludarus <burgermanjoe48@gmail.com>
2026-09-13 16:45:19 -0400

    sioyek default pdf reader config

--- .config/nvim/lua/config/commands.lua
+++ .config/nvim/lua/config/commands.lua
@@ -36,7 +36,7 @@ vim.api.nvim_create_autocmd("BufReadCmd", {
 	pattern = "*.pdf",
 	callback = function()
 		local filename = vim.fn.shellescape(vim.api.nvim_buf_get_name(0))
-		vim.cmd("silent !zen-browser " .. filename .. " &")
+		vim.cmd("silent !sioyek " .. filename .. " &")
 		vim.cmd("let tobedeleted = bufnr('%') | b# | exe \"bd! \" . tobedeleted")
 	end
 })
